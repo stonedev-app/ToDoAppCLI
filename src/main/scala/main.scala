2e6ce289cd8f5caf894b6ecce1ecd49f6cf11f72
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-02-10 17:29:14 +0900

    feat: Added list and add args

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,8 +4,21 @@ import todo.formatTodos
 import utils.loadTodos
 
 @main
-def main(): Unit = {
-  val todos = loadTodos(".todo.json")
-  formatTodos(todos).foreach(println)
+def main(args: String*): Unit = {
+  if (args.isEmpty) {
+    println("Usage: list|add [options]")
+  } else {
+    args(0) match {
+      case "list" =>
+        val todos = loadTodos(".todo.json")
+        formatTodos(todos).foreach(println)
+      case "add" =>
+        val todos = loadTodos(".todo.json")
+        // TODO: add task
+        // TODO: write json file
+      case _ =>
+        println("Unknown command. Usage: list|add [options]")
+    }
+  }
 }
 
