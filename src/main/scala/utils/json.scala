1fa65af01a98e0524e10495156cd6761aaa534c9
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-02-11 23:26:48 +0900

    feat: Add a file writing feature for ToDo

--- src/main/scala/utils/json.scala
+++ src/main/scala/utils/json.scala
@@ -1,6 +1,8 @@
 package com.app.stone.dev
 package utils
 
+import java.io.PrintWriter
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 /***
@@ -16,4 +18,38 @@ def loadTodos(filename: String): List[Map[String, ujson.Value]] = {
 
   val json = ujson.read(jsonString)
   json.arr.map(_.obj.toMap).toList
+}
+
+/***
+ * saveTodos
+ *
+ * @param todos Todos
+ * @param filename TodosFileName
+ */
+def saveTodos(todos: List[Map[String, ujson.Value]], filename: String): Unit = {
+  // list for storing jsonObj
+  val listBuffer: ListBuffer[ujson.Obj] = ListBuffer()
+
+  // Loop through todos
+  for (todo <- todos) {
+    // create jsonObj
+    val jsonObj = ujson.Obj()
+    // Transfer the keys and values from the Todo(Map) to jsonObj
+    for ((key, value) <- todo) {
+      jsonObj(key) = value
+    }
+    // Add jsonObj to the list
+    listBuffer += jsonObj
+  }
+
+  // Convert the jsonObj inside the list into a ujson.Arr
+  val json = ujson.Arr(listBuffer.toList: _*)
+
+  // Convert to a string
+  val jsonString = json.render()
+
+  // write to a file
+  val writer = new PrintWriter(filename)
+  writer.write(jsonString)
+  writer.close()
 }
