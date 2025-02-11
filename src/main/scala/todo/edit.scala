6b00973fa0dd57126743ab06a6661aefb9458de5
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-02-11 13:13:45 +0900

    feat: Add a feature to add tasks

---
+++ src/main/scala/todo/edit.scala
@@ -0,0 +1,44 @@
+package com.app.stone.dev
+package todo
+
+/***
+ * addTasks
+ * @param todos Todos
+ * @param tasks Tasks
+ * @return Todos
+ */
+def addTasks(todos: List[Map[String, ujson.Value]], tasks: String*) :List[Map[String, ujson.Value]] = {
+  // to get the starting id for adding new tasks
+  val startId = getTodosMaxId(todos) + 1
+  // Generate a list of tuples containing the tasks and id
+  val taskIdTupleList = tasks.zipWithIndex.map { case(task, index) => (task, index + startId) }
+  // Add the newly created Todos to the original Todos
+  todos ++ taskIdTupleList.map{case(task, id) => createTodo(id, task)}
+}
+
+/***
+ * createTodo
+ * @param id id
+ * @param task task
+ * @return Todo
+ */
+def createTodo(id: Int, task: String): Map[String, ujson.Value] = {
+  Map(
+    "id"   -> ujson.Num(id.toDouble),
+    "task" -> ujson.Str(task),
+    "done" -> ujson.Bool(false)
+  )
+}
+
+/***
+ * getTodosMaxId
+ * @param todos Todos
+ * @return MaxId
+ */
+def getTodosMaxId(todos: List[Map[String, ujson.Value]]) :Int = {
+  // The Todo list's ID starts from 0
+  todos.foldLeft(0) { (max, todo) =>
+    val id = todo("id").num.toInt
+    if (id > max) id else max
+  }
+}
