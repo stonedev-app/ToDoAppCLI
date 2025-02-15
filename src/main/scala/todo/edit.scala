c34e0de30477b95a5a4ac43100ffb1ecaf0a6187
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-02-15 23:01:37 +0900

    refactor: Modify it so that the Done parameter can be passed externally

--- src/main/scala/todo/edit.scala
+++ src/main/scala/todo/edit.scala
@@ -12,7 +12,7 @@ def makeAddTodos(nextId: Int,
   // Generate a list of tuples containing the title and id
   val titleIdTupleList = titles.zipWithIndex.map { case(title, index) => (title, index + nextId) }
   // Newly created Todos
-  titleIdTupleList.map{case(title, id) => createTodo(id, title)}
+  titleIdTupleList.map{case(title, id) => createTodo(id, title, false)}
 }
 
 /** *
@@ -36,12 +36,13 @@ def nextIdCalculator(startId: Int): List[Map[String, ujson.Value]] => Int = {
  * createTodo
  * @param id id
  * @param title Title
+ * @param done true:done false:not done
  * @return Todo
  */
-private def createTodo(id: Int, title: String): Map[String, ujson.Value] = {
+private def createTodo(id: Int, title: String, done: Boolean): Map[String, ujson.Value] = {
   Map(
     Keys.Id.name    -> ujson.Num(id.toDouble),
     Keys.Title.name -> ujson.Str(title),
-    Keys.Done.name  -> ujson.Bool(false)
+    Keys.Done.name  -> ujson.Bool(done)
   )
 }
