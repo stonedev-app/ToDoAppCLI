e45d03dd4f869b3cee50f1892aa2cb9754b60eaa
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-02-15 11:24:20 +0900

    refactor: Consolidate the ID retrieval process into a getNextId function

--- src/main/scala/todo/edit.scala
+++ src/main/scala/todo/edit.scala
@@ -8,28 +8,28 @@ package todo
  * @return Todos
  */
 def makeTodos(todos: List[Map[String, ujson.Value]], titles: List[String]) :List[Map[String, ujson.Value]] = {
-  // get the maxId from todos
-  val maxId = getTodosMaxId(todos)
-  // ID start at 0. If ToDos already exists, increment the ID by 1.
-  val startId = if(maxId == Int.MinValue) 0 else maxId + 1
+  // NextId
+  val nextId = getNextId(todos)
   // Generate a list of tuples containing the title and id
-  val titleIdTupleList = titles.zipWithIndex.map { case(title, index) => (title, index + startId) }
+  val titleIdTupleList = titles.zipWithIndex.map { case(title, index) => (title, index + nextId) }
   // Newly created Todos
   titleIdTupleList.map{case(title, id) => createTodo(id, title)}
 }
 
 /** *
- * getTodosMaxId
+ * getNextId
  *
  * @param todos Todos
- * @return MaxId. If todos is empty, return Int.MinValue.
+ * @return NextId. If todos is empty, return 0.
  */
-private def getTodosMaxId(todos: List[Map[String, ujson.Value]]): Int = {
-  // If todos is empty, return Int.MinValue
-  todos.foldLeft(Int.MinValue) { (max, todo) =>
+private def getNextId(todos: List[Map[String, ujson.Value]]): Int = {
+  // If todos is empty, maxId is Int.MinValue
+  val maxId = todos.foldLeft(Int.MinValue) { (max, todo) =>
     val id = todo(Keys.Id.name).num.toInt
     if (id > max) id else max
   }
+  // ID start at 0. If ToDos already exists, increment the ID by 1.
+  if(maxId == Int.MinValue) 0 else maxId + 1
 }
 
 /***
